2d5d505c2beba0060378b48d711d21a8a23327fc
kermage <genealyson.torcende@gmail.com>
2022-05-19 00:10:49 +0800

    revamp handling our constants; easier defaults management

--- src/Application.php
+++ src/Application.php
@@ -15,6 +15,9 @@ use League\Container\Container;
 
 class Application extends Container {
 
+	use Constants;
+
+
 	public function boot( string $base_path ): Application {
 
 		$dotenv = Dotenv::createImmutable( $base_path );
@@ -34,13 +37,10 @@ class Application extends Container {
 
 	protected function bootstrap_wordpress( string $base_path ): void {
 
-		// Database Settings
-		define( 'DB_NAME', Env::get( 'DB_NAME' ) ?: 'local' );
-		define( 'DB_USER', Env::get( 'DB_USER' ) ?: 'root' );
-		define( 'DB_PASSWORD', Env::get( 'DB_PASSWORD' ) ?: 'root' );
-		define( 'DB_HOST', Env::get( 'DB_HOST' ) ?: 'localhost' );
-		define( 'DB_CHARSET', Env::get( 'DB_CHARSET' ) ?: 'utf8' );
-		define( 'DB_COLLATE', Env::get( 'DB_COLLATE' ) ?: '' );
+		// Loop all must-haves with our defaults
+		foreach ( $this->get_opinionated_constants() as $constant => $default ) {
+			$this->define( $constant, $default );
+		}
 
 		// Authentication Unique Keys and Salts
 		define( 'AUTH_KEY', Env::get( 'AUTH_KEY' ) );
@@ -51,12 +51,6 @@ class Application extends Container {
 		define( 'SECURE_AUTH_SALT', Env::get( 'SECURE_AUTH_SALT' ) );
 		define( 'LOGGED_IN_SALT', Env::get( 'LOGGED_IN_SALT' ) );
 		define( 'NONCE_SALT', Env::get( 'NONCE_SALT' ) );
-
-		// Debugging Settings
-		define( 'WP_DEBUG', Env::get( 'WP_DEBUG' ) ?: false );
-		define( 'WP_DEBUG_LOG', Env::get( 'WP_DEBUG_LOG' ) ?? true );
-		define( 'WP_DEBUG_DISPLAY', Env::get( 'WP_DEBUG_DISPLAY' ) ?: false );
-		define( 'SCRIPT_DEBUG', Env::get( 'SCRIPT_DEBUG' ) ?: false );
 
 		/**
 		 * Behind load balancers or reverse proxies that support HTTP_X_FORWARDED_PROTO
@@ -69,26 +63,9 @@ class Application extends Container {
 		/**
 		 * Custom Settings
 		 */
-		define( 'WP_HOME', Env::get( 'WP_HOME' ) ?? $this->targeted_request() );
-
-		// Opinionated defaults
-		define( 'WP_ROOT_DIR', Env::get( 'WP_ROOT_DIR' ) ?? '/wp' );
-		define( 'CONTENT_DIR', Env::get( 'CONTENT_DIR' ) ?? '/content' );
-
-		define( 'WP_SITEURL', Env::get( 'WP_SITEURL' ) ?: WP_HOME . WP_ROOT_DIR );
-		define( 'WP_CONTENT_DIR', Env::get( 'WP_CONTENT_DIR' ) ?: $base_path . CONTENT_DIR );
-		define( 'WP_CONTENT_URL', Env::get( 'WP_CONTENT_URL' ) ?: WP_HOME . CONTENT_DIR );
-
-		define( 'WP_ENVIRONMENT_TYPE', Env::get( 'WP_ENVIRONMENT_TYPE' ) ?: 'local' );
-		define( 'WP_DEFAULT_THEME', Env::get( 'WP_DEFAULT_THEME' ) ?? '' );
-
-		define( 'AUTOMATIC_UPDATER_DISABLED', Env::get( 'AUTOMATIC_UPDATER_DISABLED' ) ?: true );
-		define( 'DISABLE_WP_CRON', Env::get( 'DISABLE_WP_CRON' ) ?: false );
-		define( 'DISALLOW_FILE_EDIT', Env::get( 'DISALLOW_FILE_EDIT' ) ?: true );
-
-		$disabled_updates = in_array( strtolower( WP_ENVIRONMENT_TYPE ), array( 'staging', 'production' ), true );
-
-		define( 'DISALLOW_FILE_MODS', Env::get( 'DISALLOW_FILE_MODS' ) ?: $disabled_updates );
+		foreach ( $this->get_custom_constants( $base_path ) as $constant => $default ) {
+			$this->define( $constant, $default );
+		}
 
 		$local_config = $base_path . '/wp-config-local.php';
 
@@ -100,21 +77,6 @@ class Application extends Container {
 		if ( ! defined( 'ABSPATH' ) ) {
 			define( 'ABSPATH', $base_path . WP_ROOT_DIR );
 		}
-
-	}
-
-
-	protected function targeted_request(): string {
-
-		$protocol = 'http';
-
-		if ( isset( $_SERVER['HTTPS'] ) && in_array( strtolower( $_SERVER['HTTPS'] ), array( '1', 'on' ), true ) ) {
-			$protocol .= 's';
-		} elseif ( isset( $_SERVER['SERVER_PORT'] ) && ( '443' === $_SERVER['SERVER_PORT'] ) ) {
-			$protocol .= 's';
-		}
-
-		return $protocol . '://' . ( $_SERVER['HTTP_HOST'] ?? 'localhost' );
 
 	}
 
---
+++ src/Constants.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @package ThemePlate
+ */
+
+namespace ThemePlate;
+
+use Env\Env;
+
+trait Constants {
+
+	protected static string $default_base_path   = __DIR__;
+	protected static string $default_wp_root_dir = '/wp';
+	protected static string $default_content_dir = '/content';
+	protected static string $default_wp_env_type = 'local';
+	protected static string $default_wp_theme    = '';
+
+
+	protected function get_opinionated_constants(): array {
+
+		return array(
+			'DB_NAME'             => 'local',
+			'DB_USER'             => 'root',
+			'DB_PASSWORD'         => 'root',
+			'DB_HOST'             => 'localhost',
+			'DB_CHARSET'          => 'utf8',
+			'DB_COLLATE'          => '',
+			'WP_DEBUG'            => false,
+			'WP_DEBUG_LOG'        => array( true ),
+			'WP_DEBUG_DISPLAY'    => false,
+			'SCRIPT_DEBUG'        => false,
+			'WP_HOME'             => array( $this->targeted_request() ),
+			'WP_ROOT_DIR'         => array( self::$default_wp_root_dir ),
+			'CONTENT_DIR'         => array( self::$default_content_dir ),
+			'WP_ENVIRONMENT_TYPE' => self::$default_wp_env_type,
+			'WP_DEFAULT_THEME'    => array( self::$default_wp_theme ),
+		);
+
+	}
+
+
+	protected function get_custom_constants( string $base_path ): array {
+
+		return array(
+			'WP_SITEURL'                 => WP_HOME . WP_ROOT_DIR,
+			'WP_CONTENT_DIR'             => $base_path . CONTENT_DIR,
+			'WP_CONTENT_URL'             => WP_HOME . CONTENT_DIR,
+			'AUTOMATIC_UPDATER_DISABLED' => true,
+			'DISABLE_WP_CRON'            => false,
+			'DISALLOW_FILE_EDIT'         => true,
+			'DISALLOW_FILE_MODS'         => $this->is_disabled_updates(),
+		);
+
+	}
+
+
+	protected function targeted_request(): string {
+
+		$protocol = 'http';
+
+		if ( isset( $_SERVER['HTTPS'] ) && in_array( strtolower( $_SERVER['HTTPS'] ), array( '1', 'on' ), true ) ) {
+			$protocol .= 's';
+		} elseif ( isset( $_SERVER['SERVER_PORT'] ) && ( '443' === $_SERVER['SERVER_PORT'] ) ) {
+			$protocol .= 's';
+		}
+
+		return $protocol . '://' . ( $_SERVER['HTTP_HOST'] ?? 'localhost' );
+
+	}
+
+
+	protected function is_disabled_updates(): bool {
+
+		return in_array( strtolower( WP_ENVIRONMENT_TYPE ), array( 'staging', 'production' ), true );
+
+	}
+
+
+	/**
+	 * @param string $constant
+	 * @param mixed  $default  (boolean | string)
+	 * If value is an array, the comparison operator
+	 * is switched from 'null-coalescing' to 'ternary'
+	 *
+	 * @return void
+	 */
+	protected function define( string $constant, $default ): void {
+
+		if ( is_array( $default ) ) {
+			define( $constant, Env::get( $constant ) ?? $default[0] );
+		} else {
+			// phpcs:ignore WordPress.PHP.DisallowShortTernary
+			define( $constant, Env::get( $constant ) ?: $default );
+		}
+
+	}
+
+}
